3bc1a629cd5e84a62a8a682742d46ec225379e71
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-09 22:07:00 -0500

    count tickets by weekday

--- data/clean_data.py
+++ data/clean_data.py
@@ -14,18 +14,22 @@ import datetime as dt
 def weekdays(df, col_name, date_format='%Y%m%d'):
     ''' Pull weekday from timestamp '''
     format_date = dt.datetime.strptime(str(df[col_name]), date_format)
-    return format_date
+    return format_date.strftime('%A')
 
 
-def tickets_by_day(data):
-    pass
+def ticket_count(df, col_name, date_format):
+    df['weekday'] = df.apply(weekdays(df, col_name), axis=1)
+    weekday_counts = df['weekday'].value_counts()
+    return weekday_counts 
 
 
 def main():
     df = pd.read_csv('data/Parking_Tags_Data_2012.csv')
     col_name = 'date_of_infraction'
+    date_format = '%Y%m%d'
+    weekday_counts = ticket_count(df, col_name, date_format)
 
-    print(df.dtypes)
+    print(weekday_counts)
 
 
 if __name__ == '__main__':
